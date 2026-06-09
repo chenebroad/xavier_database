870e41dfba345e7070b256871496d188bce7ad44
Eric Chen <chene@broadinstitute.org>
2026-06-09 16:02:05 -0400

    Update main_page.py

--- userapp/pages/main_page.py
+++ userapp/pages/main_page.py
@@ -4,5 +4,7 @@ import pandas as pd
 st.title("LIMS Dashboard")
 
 st.write("Use the sidebar to navigate:")
+st.write("How to ingest data into the LIMS system")
+st.write("How to query data in the LIMS system")
 
 st.sidebar.markdown("Main page with instructions")
