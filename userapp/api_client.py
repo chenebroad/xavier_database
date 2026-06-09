2ca35110ef43b8629c2c060ec2647d8a8ca0d3db
Eric Chen <chene@broadinstitute.org>
2026-06-09 13:52:42 -0400

    Setting up Streamlit on Cloud

--- userapp/api_client.py
+++ userapp/api_client.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-BASE_URL = "http://localhost:8000"
+BASE_URL = "https://xavier-db-dev-134042435125.us-central1.run.app"
 
 # -------------------------
 # Helper for POST requests
