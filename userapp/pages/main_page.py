102e67fe87715ab6fb43bff1712133fc0855fc06
Eric Chen <chene@broadinstitute.org>
2026-06-10 10:50:45 -0400

    Update main_page.py

--- userapp/pages/main_page.py
+++ userapp/pages/main_page.py
@@ -6,5 +6,6 @@ st.title("LIMS Dashboard")
 st.write("Use the sidebar to navigate:")
 st.write("How to ingest data into the LIMS system")
 st.write("How to query data in the LIMS system")
+st.write("Consult the data dictionary to get a definition of the schema and the columns")
 
 st.sidebar.markdown("Main page with instructions")
