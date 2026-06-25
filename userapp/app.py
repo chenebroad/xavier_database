f526dc71471585eeaf8469ad4e5663eb85ee9683
Eric Chen <chene@broadinstitute.org>
2026-06-25 16:03:24 -0400

    page update, erd moved

--- userapp/app.py
+++ userapp/app.py
@@ -2,16 +2,15 @@ from pathlib import Path
 import streamlit as st
 
 #Navigation only
-html_content = (Path(__file__).parent / "assets/xavier_erd_panel.html").read_text()
-st.components.v1.html(html_content, height=750, scrolling=False)
-
 st.set_page_config(page_title="LIMS", layout="wide")
 
 main_page = st.Page ("pages/main_page.py", title="Main Page")
 page2 = st.Page("pages/ingest_data.py", title="Ingest")
 page3 = st.Page("pages/query_data.py", title="Query")
 page4 = st.Page("pages/data_dictionary.py", title="Data Dictionary")
-pg = st.navigation([main_page, page2, page3, page4])
+page5 = st.Page("pages/dashboard.py", title="Dashboard")
+page6 = st.Page("pages/reference.py", title="Reference ERD")
+pg = st.navigation([main_page, page2, page3, page4, page5])
 
 pg.run()
 
