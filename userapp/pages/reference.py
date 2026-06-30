c21d6b97a7a4eec8d00a1f6965634f75a3a970d4
Eric Chen <chene@broadinstitute.org>
2026-06-30 14:51:09 -0400

    Update reference.py

--- userapp/pages/reference.py
+++ userapp/pages/reference.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from pathlib import Path
 
-html_content = ("../assets/xavier_erd_panel.html").read_text()
+html_content = ("../assets/xavier_erd_panel.html")
 st.components.v1.html(html_content, height=750, scrolling=False)
