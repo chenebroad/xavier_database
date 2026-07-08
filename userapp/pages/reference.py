44c8398256e29d3effe9e275938ea324fceb1930
Eric Chen <chene@broadinstitute.org>
2026-07-08 15:28:59 -0400

    Reference fix

--- userapp/pages/reference.py
+++ userapp/pages/reference.py
@@ -1,5 +1,5 @@
 import streamlit as st
 from pathlib import Path
 
-html_content = ("../assets/xavier_erd_panel.html")
-st.components.v1.html(html_content, height=750, scrolling=False)
+html_content = (Path(__file__).parent.parent / "assets" / "xavier_erd_panel.html").read_text()
+st.components.v1.html(html_content, height=800, scrolling=True)
